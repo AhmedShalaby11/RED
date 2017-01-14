c6fe86813e3bf560128be84f037a99e4a01237f4
Ahmed Shalaby <AhmedShalaby@MAC-OSX.local>
2017-01-14 02:13:08 +0200

    1.copy text/row function \n 2.simple design.

--- home.php
+++ home.php
@@ -49,6 +49,7 @@ echo( '<nav class="navbar navbar-default">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">RegGate</a>
     </div>
+    
     <ul class="nav navbar-nav" id="li1s">
      '.$row[0].'
     </ul>
