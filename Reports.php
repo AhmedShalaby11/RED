383c9a3bd0b21867efb28e371d26054db85140c4
Ahmed Shalaby <AhmedShalaby@MAC-OSX.local>
2017-02-02 14:00:08 +0200

    reporting done

--- Reports.php
+++ Reports.php
@@ -1,7 +1,11 @@
 
+<table>
+
+<tr>
 
 <div class="container" id='firstReport' >
-  <div id='welcomePanel' class='bs-callout bs-callout-danger' > <h4>Report1</h4> <p>Shows Number of customers to each GigabitEthernet</p> </div>
-  <iframe id="frame1"  src="getreport.php" width="70%" height="400px" style="border-color:white;border-width:0px;" sandbox="allow-scripts"></iframe>
+  <iframe id="frame1"  src="getreport.php" width="100%" height="400px" style="border-color:white;border-width:0px;" sandbox="allow-scripts"></iframe>
 
 </div>
+</tr>
+</table>
--- getReport.php
+++ getReport.php
@@ -1,23 +1,37 @@
 <?php
-
+include 'lib1.php';
+$username  = gethostname();
+$databae = 'redgate';
+$table = 'table_trend_config';
 include 'dbconfig.php';
-$sql1 = "select distinct (old_interface) ,count(old_interface) from redgate.table_id_log group by old_interface ;";
+//query 1
+//changes div.name 3
+//chart.name 3
+//js chartname 3
+//chart title 2
+//chart type 4
+//column schema 5
+//---------------
+
+$sql_getChartConfig = "select * from $database.$table ;";
+$chartConfig= $conn->query($sql_getChartConfig);
+while ($config = $chartConfig -> fetch_array(MYSQLI_NUM)) {
+$sql1 =$config[1];
+
 $res  = $conn->query($sql1);
 echo "
 
+<td>
+<div id='welcomePanel' class='bs-callout bs-callout-danger' > <h3>".$config[2]."</h3> <p></p> </div>
+<div id='".$config[3]."' style=' height:300px;'>
+</td>
 
-
-<div id='firstReport' style='height: 300px; width: 100%;'>
-
-
-
-
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 <script src='http://canvasjs.com/assets/script/canvasjs.min.js'></script>
 <script type='text/javascript'>
-
-window.onload = function () {
-	var chart = new CanvasJS.Chart('firstReport', {
-		theme: 'theme2',//theme1
+$(document).ready(function () {
+	var ".$config[3]." = new CanvasJS.Chart('".$config[3]."', {
+		theme: 'theme1',//theme1
 		title:{
 			text: 'Interface to Customer - Test'
 		},
@@ -25,27 +39,27 @@ window.onload = function () {
 		data: [
 		{
 			// Change type to 'bar', 'area', 'spline', 'pie',etc.
-			type: 'bar',
+			type: '".$config[4]."',
 			dataPoints: [";
 while ($row = $res->fetch_array(MYSQLI_NUM))
 {
 	echo "{ label: '$row[0]',  y: $row[1]  },";
 }
-
-
-
 			echo "]
 		}
 		]
 	});
-	chart.render();
-}
+	".$config[3].".render();
+});
 </script>
-
-
-
 </div>
+
+<script>
+$(document).ready(function(){
+  $('.canvasjs-chart-credit').text('Integration Management Reporting');
+  $('.canvasjs-chart-credit').css({'color':'red','font-size':'large'});
+});
+</script>
 ";
-
-
+}
  ?>
